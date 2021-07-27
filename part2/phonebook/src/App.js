faac8f8f8646f332b3a4bab2df17bb17240abf93
ChristopherGustafson <christopher.gustafson@outlook.com>
2021-07-27 11:43:08 +0200

    Finished first phonebook exercs without server

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,62 @@
+import React, {useState} from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: "Arto Hellas", number: "040-123456"},
+    {name: "Ada Lovelace", number: "39-44-5323523"},
+    {name: "Dan Abramov", number: "12-43-234345"},
+    {name: "Mary Poppendieck", number: "39-23-6423122"},
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`);
+    } else {
+      setPersons(persons.concat({name: newName, number: newNumber}));
+    }
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <h2>Add a new</h2>
+      <PersonForm
+        onSubmit={addPerson}
+        name={newName}
+        number={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+
+      <Persons persons={persons} filter={filter} />
+    </div>
+  );
+};
+
+export default App;
