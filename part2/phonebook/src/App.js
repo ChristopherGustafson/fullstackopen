42239336425fb426e3c920fa205e6594387235dc
ChristopherGustafson <christopher.gustafson@outlook.com>
2021-07-29 13:01:43 +0200

    Changed error handling to be shown with notification

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,12 +6,7 @@ import PersonService from "./services/personService";
 import Notification from "./components/Notification";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {name: "Arto Hellas", number: "040-123456"},
-    {name: "Ada Lovelace", number: "39-44-5323523"},
-    {name: "Dan Abramov", number: "12-43-234345"},
-    {name: "Mary Poppendieck", number: "39-23-6423122"},
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
@@ -71,7 +66,7 @@ const App = () => {
           );
         })
         .catch((error) => {
-          console.log("Error, ", error);
+          showMessage(error.response.data, "error");
         });
     }
 
