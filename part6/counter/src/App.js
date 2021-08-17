41102a4b879b8559dc91cd1cf324fea63d89928c
ChristopherGustafson <christopher.gustafson@outlook.com>
2021-08-17 13:11:19 +0200

    finished simple counter using redux

--- part6/counter/src/App.js
+++ part6/counter/src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
+import {createStore} from "redux";
 
-function App() {
+const App = () => {
+  const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+      case "ZERO":
+        return 0;
+      default:
+        return state;
+    }
+  };
+
+  const store = createStore(counterReducer);
+
   return (
-    <div className="App">
-      <h1>Counter</h1>
+    <div>
+      <div>{store.getState()}</div>
+      <button onClick={(e) => store.dispatch({type: "INCREMENT"})}>plus</button>
+      <button onClick={(e) => store.dispatch({type: "DECREMENT"})}>
+        minus
+      </button>
+      <button onClick={(e) => store.dispatch({type: "ZERO"})}>zero</button>
     </div>
   );
-}
+};
 
 export default App;
