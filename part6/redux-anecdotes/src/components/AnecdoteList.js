3ff690a7b73e76d5821d0148b617e58a8109a7d1
ChristopherGustafson <christopher.gustafson@outlook.com>
2021-08-18 11:07:10 +0200

    Added filtering of anecdotes

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -6,7 +6,11 @@ import {
 } from "../reducers/notificationReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state.anecdotes);
+  const anecdotes = useSelector((state) => {
+    return state.anecdotes.filter((anecdote) =>
+      anecdote.content.toLowerCase().includes(state.filter.toLowerCase())
+    );
+  });
   const dispatch = useDispatch();
 
   const vote = (id, content) => {
